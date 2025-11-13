8034018e7654c031c194dc4f1f4e3fecd6a72e29
kishorevutukury <jayasaikishore@gmail.com>
2025-11-13 13:14:13 -0800

    cleanUp

--- src/main/java/com/leancam/employeemanager/controller/EmployeeResource.java
+++ src/main/java/com/leancam/employeemanager/controller/EmployeeResource.java
@@ -3,7 +3,6 @@ package com.leancam.employeemanager.controller;
 import com.leancam.employeemanager.model.Employee;
 import com.leancam.employeemanager.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
